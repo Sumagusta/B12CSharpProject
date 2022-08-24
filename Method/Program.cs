886b32aa794dafe336ea23fafc95b52004b751c1
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2022-08-24 17:06:48 +0700

    Try Catch Exception

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Oop.constructor;
 using Oop.Core;
+using Oop.exceptionhandling;
 using Oop.generic;
 using Oop.Inheritance.extend;
 using Oop.Inheritance.impl;
@@ -93,7 +94,33 @@ namespace Oop
             Console.WriteLine(print.nominal<string>("Lima Ratus"));
             Console.WriteLine(print.nominal<int>(500));
 
-            // generic interface
+            // try catch
+
+            TryCatch tryObj = new TryCatch();
+            int angka1 = 10;
+            int angka2 = 0;
+            int[] arr = new int[2];
+            //Console.WriteLine(tryObj.aritmatika(10, 0));
+            try
+            {
+                
+                /*arr[0] = 2;
+                arr[1] = 3;
+                arr[2] = 4;*/
+
+                Console.WriteLine(tryObj.aritmatika(angka1, angka2));
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine("pembagi {0} tidak boleh bernilai 0, error : {1}", angka2, e.Message);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Index Array tidak boleh > {0}, error : {1}", arr.Length, e.Message);
+
+            }
+
+            Console.WriteLine("Fungsi yang dijalankan setelah aritmatika ..");
 
         }
     }
---
+++ Method/exceptionhandling/TryCatch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop.exceptionhandling
+{
+    class TryCatch
+    {
+
+        public int aritmatika(int angka1, int angka2)
+        {
+            return angka1 / angka2;
+        }
+
+    }
+}
